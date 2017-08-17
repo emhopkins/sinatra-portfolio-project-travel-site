0ff89f75311982368a0f51b3076a619418b3888b
emhopkins <epoletto@lstreetc.com>
2017-08-16 22:53:27 -0500

    update to user model and user routes in application controller

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,65 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+ 	configure do
+	    set :public_folder, 'public'
+	    set :views, 'app/views'
+	    enable :sessions
+		set :session_secret, "password_security"
+	end
+
+	get "/" do
+		erb :index
+	end
+
+	get "/signup" do
+		erb :signup
+	end
+
+	post "/signup" do
+	    if !params[:username].empty? && !params[:email].empty? && !params[:password].empty?
+	    	user = User.create(:username => params[:username], :email => params[:email], :password => params[:password])
+	    	session[:user_id] = user.id
+	    	redirect "homepage"
+		else
+	    	redirect "/signup"
+		end
+	end
+
+
+	get "/login" do
+		erb :login
+	end
+
+	post "/login" do
+		user = User.find_by(:username => params[:username])
+	    if user && user.authenticate(params[:password])
+	        session[:user_id] = user.id
+	        redirect "homepage"
+	    else
+	        redirect "/login"
+	    end
+	end
+
+	get "/logout" do
+		session.clear
+		redirect "/login"
+	end
+
+
+    # get "/users/:slug" do 
+    # 	erb :'/users/show'
+    # end
+
+	helpers do
+		def logged_in?
+			!!session[:user_id]
+		end
+
+		def current_user
+			User.find(session[:user_id])
+		end
+	end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,20 @@ class User < ActiveRecord::Base
     has_secure_password
     has_many :destinations
     has_many :activities
+
+    def slug
+		a = []
+		a = self.username.split(" ")
+		a.join("-").downcase
+	end
+
+	def self.find_by_slug(slug)
+		a = []
+		name = ""
+		a = slug.split("-")
+		a.each do |s|
+			name << s + " "
+		end
+		User.find_by(username: name.strip)
+	end
 end
